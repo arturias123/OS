abfc6de66a8f9f6360e798bd7329210bd511ed75
Minh Đức Cao <34432166+DucMinhCao@users.noreply.github.com>
2020-10-14 22:19:42 +0700

    Update readline.h

--- readline.h
+++ readline.h
@@ -1,6 +1,8 @@
+#pragma once
 #ifndef READ_LINE_H
 #define READ_LINE_H
 
-int read_line(char *str);
-
+int read_line(char* str);
+char* substring(char* destination, const char* source, int beg, int n);
+void substring(char* source, int from, int n, char* target);
 #endif
