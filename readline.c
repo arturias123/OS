eec247a2f3292726ac964a53b3674fc5a06ec36d
Minh Đức Cao <34432166+DucMinhCao@users.noreply.github.com>
2020-10-14 23:13:29 +0700

    Update readline.c

--- readline.c
+++ readline.c
@@ -1,28 +1,48 @@
 #include "readline.h"
 #include <stdio.h>
 #include <string.h>
+#pragma warning(disable:4996)
 
-int read_line(char *str) {
-	int i = 0;
-	int sum = 0;
-	char realstr[100];
-	char c;
-	printf("Enter string: ");
-	do {
+
+int read_line(char* &str) {
+    int i = 0;
+    int sum = 0;
+    char realstr[100];
+    char c;
+    printf("Enter string from readline: ");
+    scanf("\n");
+    do {
         c = getchar();
         realstr[i] = c;
         i++;
         if (c == '\n') {
-        	str = realstr;
-            printf("%s", str);
-            printf("%d\n", i - 1);
-            i = 0;
-            str[0] = '\0';
+            substring(realstr, 0, i, str);
+            return strlen(str) - 1;
         }
         if (c == EOF) {
-        	str = realstr;
-            printf("\n%s\n", str);
-            printf("%d\n", -1);
+            substring(realstr, 0, i, str);
+            return -1;
         }
-    } while (c != EOF && c!= '\n');
+    } while (c != EOF && c != '\n');
+}
+
+void substring(char* source, int from, int n, char* target) {
+    int length, i;
+    for (length = 0; source[length] != '\0'; length++);
+
+    if (from > length) {
+        printf("Starting index is invalid.\n");
+        return;
+    }
+
+    if ((from + n) > length) {
+        n = (length - from);
+    }
+
+    for (i = 0; i < n; i++) {
+        target[i] = source[from + i];
+    }
+    target[i] = '\0';
+
+    return;
 }
